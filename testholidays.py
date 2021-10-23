ae2b63336f3bfe8785edf4757b9775a3b803877c
Don Bower <don.bower@outlook.com>
2021-10-23 09:22:51 -0700

    test

--- testholidays.py
+++ testholidays.py
@@ -1,6 +1,11 @@
 from datetime import date
 import holidays
-us_holidays = holidays.UnitedStates()
+us_holidays = holidays.UnitedStates(observed=False)
 
 for date, name in sorted(holidays.US(years=2021).items()):
+  print(date, name)
+
+myHolidays = holidays.UnitedStates(years=[2021,2022],observed=False)
+
+for date, name in sorted(myHolidays.items()):
   print(date, name)
