3a40a9a8135aa1411d594dba0b1b8cb799bc466c
Don Bower <don.bower@outlook.com>
2021-10-23 09:29:17 -0700

    remove unused

--- testholidays.py
+++ testholidays.py
@@ -6,6 +6,27 @@ for date, name in sorted(holidays.US(years=2021).items()):
   print(date, name)
 
 myHolidays = holidays.UnitedStates(years=[2021,2022],observed=False)
+myHolidays.pop_named("Martin Luther King")
+myHolidays.pop_named("Columbus Day")
+myHolidays.pop_named("Memorial")
+myHolidays.pop_named("Juneteenth")
+myHolidays.pop_named("Independence")
+myHolidays.pop_named("Labor")
+myHolidays.pop_named("Veterans")
+
 
 for date, name in sorted(myHolidays.items()):
   print(date, name)
+
+
+
+# 2021-01-01 New Year's Day
+# 2021-01-18 Martin Luther King Jr. Day
+# 2021-02-15 Washington's Birthday
+# 2021-05-31 Memorial Day
+# 2021-06-19 Juneteenth National Independence Day
+# 2021-07-04 Independence Day
+# 2021-07-05 Independence Day (Observed)
+# 2021-09-06 Labor Day
+# 2021-10-11 Columbus Day
+# 2021-11-11 Veterans Day
